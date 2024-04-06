21a8417570b13330bad11129de4b78e0891aff93
Kody Wells <kodyswells@gmail.com>
2024-04-06 10:27:06 -0600

    added model files

---
+++ Server/database.js
@@ -0,0 +1,9 @@
+require('dotenv').config()
+const {CONNECTION_STRING} = process.env
+const Sequelize = require('sequelize')
+
+
+const sequelize = new Sequelize(CONNECTION_STRING)
+
+
+module.exports = sequelize
--- Server/server.js
+++ Server/server.js
@@ -6,3 +6,5 @@ const app = express()
 
 app.use(express.json())
 app.use(cors())
+
+app.listen(7000, () => console.log("up on port 7000"));
